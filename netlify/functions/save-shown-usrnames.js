5c604c9648fbcd55ea7b4a6e410b0cd4e9dc8da8
abbeqdam <james.eqdam@coupa.com>
2025-01-11 12:37:37 +0100

    removed test

--- netlify/functions/save-shown-usrnames.js
+++ netlify/functions/save-shown-usrnames.js
@@ -1,32 +1,9 @@
-const fs = require('fs');
-
-exports.handler = async function (event, context) {
-  try {
-    // Get the temporary directory path
-    const tempDir = context.tempdir;
-    const filePath = tempDir + '/shown_usrnames.json';
-
-    // Attempt to write "test" to the file in the temporary directory
-    fs.writeFileSync(filePath, "test", 'utf8');
-
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ message: 'Test string written to temporary directory' }),
-    };
-  } catch (error) {
-    console.error('Error in function:', error);
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ error: 'Failed to write to file' }),
-    };
-  }
-};
-/* const fs = require('fs'); 
+ const fs = require('fs'); 
 exports.handler = async function (event, context) {
   try {
     const shownUsrnames = JSON.parse(event.body);
     const data = JSON.stringify(shownUsrnames);
-    fs.writeFileSync('shown_usrnames.json', "test", 'utf8');
+    fs.writeFileSync('shown_usrnames.json', data, 'utf8');
     return {
       statusCode: 200,
       body: JSON.stringify({ message: 'Shown usernames saved successfully' }),
@@ -38,4 +15,4 @@ exports.handler = async function (event, context) {
       body: JSON.stringify({ error: 'Failed to save shown usernames' }),
     };
   }
-}; */
+}; 
